81bde33a94f9121497b23fb730b97362184868bc
harshali444 <jadhav.h0564@gmail.com>
2020-09-10 21:57:34 +0530

    Employee is present or absent

---
+++ EmployeeWage.java
@@ -0,0 +1,14 @@
+public class EmployeeWage
+{
+        public static void main(String[] args)
+        {
+                int isFulltime=1;
+
+                double empcheck=Math.floor(Math.random()*10)%2;
+
+                if(empcheck==isFulltime)
+                System.out.println("Employee is present");
+                else
+                System.out.println("Employee is absent");
+        }
+}
